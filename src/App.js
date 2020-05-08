f7d5ff0957832fdba5d8a5dd1270c8ca1ff5ca20
tylerphopho <tyler.phopho@gmail.com>
2020-05-07 18:00:10 -0600

    creating basic page layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar"
 import Card from "./components/Card";
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import "materialize-css/dist/css/materialize.min.css";
+import Skills from './pages/Skills';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
+    <Router>
+      <Navbar navClick/>
       <div className="container">
         <Card/>
+        <Route exact path="/skills" component={Skills}/>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 import "materialize-css/dist/css/materialize.min.css";
+import Skills from "../pages/Skills";
 
 export default class Navbar extends Component {
+    navClick = event => {
+        this.setState({
+            about: "",
+            contact: "",
+            portfolio: "",
+            [event.target.name]: "active"
+        })
+    }
+
     render() {
         return (
             <nav className="nav-wrapper deep-purple darken-4">
                 <div className="container">
                 <a href="#" className="brand-logo">Tyler Pho</a>
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="#">Skills</a></li>
+                        <li className={Skills}><a href="#">Skills</a></li>
                         <li><a href="#">Porfolio</a></li>
                         <li><a href="#">Resume</a></li>
                         <li><a href="#">Contact</a></li>
