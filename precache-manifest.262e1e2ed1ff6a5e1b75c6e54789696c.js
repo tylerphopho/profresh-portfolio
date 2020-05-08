022278307da0d3ae1782fe0319fd5d33c7dbb82a
tylerphopho <tyler.phopho@gmail.com>
2020-05-07 21:38:30 -0600

    Updates

---
+++ precache-manifest.262e1e2ed1ff6a5e1b75c6e54789696c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bede23089c1f67c2c6d7ea13dbbbaa0",
+    "url": "/profresh-portfolio/index.html"
+  },
+  {
+    "revision": "8d5e7d0435ee6532aa0f",
+    "url": "/profresh-portfolio/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "f9a5d46a33b43ff30962",
+    "url": "/profresh-portfolio/static/css/main.4ab5e7ef.chunk.css"
+  },
+  {
+    "revision": "8d5e7d0435ee6532aa0f",
+    "url": "/profresh-portfolio/static/js/2.9a0fcf85.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/profresh-portfolio/static/js/2.9a0fcf85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a5d46a33b43ff30962",
+    "url": "/profresh-portfolio/static/js/main.a3f8e413.chunk.js"
+  },
+  {
+    "revision": "7febca2cdc7059aa7719",
+    "url": "/profresh-portfolio/static/js/runtime-main.214a1217.js"
+  },
+  {
+    "revision": "3c58d7f2d26c33432727bcfff653760c",
+    "url": "/profresh-portfolio/static/media/Resume.docx.3c58d7f2.pdf"
+  },
+  {
+    "revision": "fc81a3747b9f28afa15bad260738227f",
+    "url": "/profresh-portfolio/static/media/day-planner.fc81a374.PNG"
+  },
+  {
+    "revision": "44fd180be118ca1f5481648c0b40e600",
+    "url": "/profresh-portfolio/static/media/event-finder-img.44fd180b.png"
+  },
+  {
+    "revision": "e056b9e818966097408cfe8890057496",
+    "url": "/profresh-portfolio/static/media/fitness-tracker.e056b9e8.PNG"
+  },
+  {
+    "revision": "6576fbc93327f1b58fec4bc8eba3f837",
+    "url": "/profresh-portfolio/static/media/me.6576fbc9.jpg"
+  },
+  {
+    "revision": "837e1759b04455934fa10b2ae8d84c6e",
+    "url": "/profresh-portfolio/static/media/password-generator.837e1759.png"
+  },
+  {
+    "revision": "16222a645c45d5a5e190803957a20d3f",
+    "url": "/profresh-portfolio/static/media/quiz-game.16222a64.png"
+  },
+  {
+    "revision": "b5e064dd32a95eaca26d98ba5c24825b",
+    "url": "/profresh-portfolio/static/media/weather-dashboard.b5e064dd.PNG"
+  }
+]);
